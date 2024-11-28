df6775343779119d7298170d0318b0ab0d28b463
AshTheDergy <139553561+AshTheDergy@users.noreply.github.com>
2024-11-28 15:14:41 +0200

    k

--- main.py
+++ main.py
@@ -4,4 +4,27 @@ import os
 
 clear = lambda: os.system('clear')
 
-hero = {}
+hero = {
+    'hp': 100,
+    'xp': 0,
+    'items': 0
+}
+day = 1
+
+def startGame():
+	global day
+	while day <= 10:
+		print("Day", day, "started...")
+		day += 1
+
+def gameOver():
+    s
+
+def endGame():
+    s
+
+def reset():
+    n
+
+
+startGame()
